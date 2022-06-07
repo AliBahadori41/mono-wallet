38fd3e116f9c6c871ae653a4b72c17e69672c238
Ali Bahadori <ali.bahadori41@yahoo.com>
2022-06-07 12:20:19 +0430

    Create wallets using seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(10)->create();
+        User::factory(5)->create()->each(function (User $user) {
+            $user->wallet()->create();
+        });
     }
 }
