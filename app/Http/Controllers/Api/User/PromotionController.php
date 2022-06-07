e63650ed45f0aea93c40dce297361be1309cd65e
Ali Bahadori <ali.bahadori41@yahoo.com>
2022-06-07 09:56:33 +0430

    improve response code

--- app/Http/Controllers/Api/User/PromotionController.php
+++ app/Http/Controllers/Api/User/PromotionController.php
@@ -6,52 +6,42 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\User\PromotionStoreRequest;
 use App\Http\Resources\Api\PromotionResource;
 use App\Models\PromotionCode;
-use Illuminate\Http\Request;
 
 class PromotionController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
         $promotions = PromotionCode::all();
 
-        return $this->responsed([
-            'success' => true,
-            'data' => PromotionResource::collection($promotions->load('users')),
-        ]);
+        return $this->responsed(PromotionResource::collection($promotions->load('users')), true);
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\PromotionStoreRequest  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(PromotionStoreRequest $request)
     {
         $promotion = PromotionCode::create($request->all());
 
-        return $this->responsed([
-            'success' => true,
-            'data' => new PromotionResource($promotion),
-        ]);
+        return $this->responsed(new PromotionResource($promotion), true);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Model\PromotionCode $promotion_code
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show(PromotionCode $promotion_code)
     {
-        return $this->responsed([
-            'success' => true,
-            'data' => new PromotionResource($promotion_code->load('users')),
-        ]);
+        return $this->responsed(new PromotionResource($promotion_code->load('users')), true);
     }
 }
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -15,13 +15,19 @@ class Controller extends BaseController
      * Response for the Api request
      *
      * @param mixed $data
+     * @param bool $success
      * @param int $statusCode
      * @param array $header
      *
      * @return Illuminate\Http\JsonResponse
      */
-    public function responsed($data, $statusCode = 200, array $header = [])
+    public function responsed($data, bool $success, int $statusCode = 200, array $header = [])
     {
+        $data = [
+            'success' => $success,
+            'data' => $data,
+        ];
+
         return response()->json($data, $statusCode, $header);
     }
 }
